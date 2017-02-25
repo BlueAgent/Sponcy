04b7b2ba7db7b467cd986c068e3cd597f584dde3
BlueAgent <BlueAgent@users.noreply.github.com>
2017-02-25 15:31:28 +0800

    Compiles Hello World

---
+++ src/main/java/spontaneouscollection/SCConfig.java
@@ -0,0 +1,6 @@
+package spontaneouscollection;
+
+public class SCConfig {
+    public static final String MODID = "spontaneouscollection";
+    public static final String VERSION = "1.0";
+}
---
+++ src/main/java/spontaneouscollection/SpontaneousCollection.java
@@ -0,0 +1,15 @@
+package spontaneouscollection;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SCConfig.MODID, version = SCConfig.VERSION)
+public class SpontaneousCollection {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        System.out.println("Hello World");
+        System.out.printf("Mod ID: %s%n", SCConfig.MODID);
+        System.out.printf("Version: %s%n", SCConfig.VERSION);
+    }
+}
